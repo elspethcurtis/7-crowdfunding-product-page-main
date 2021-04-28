6155ff9916fb437784f1899bfb064304a1cd6eef
elspeth curtis <elspethcurtis@Elspeths-MacBook-Pro.local>
2021-04-28 19:34:58 -0400

    updated javascript to make more of the features work and worked on the css to more closely match the specs

--- main.js
+++ main.js
@@ -63,30 +63,36 @@ modalClose.addEventListener("click", () => {
         selectedRadioBtn = $("input[name='pledge']:checked").val();
         //if selected value is bamboo show bamboo enter pledge div
           if(selectedRadioBtn == "Bamboo Stand"){
-            document.querySelector(".bambooEnterPledge").style.display = "block";
+            document.querySelector(".bambooEnterPledge").style.display = "flex";
             //add green border to selected radio button
             $(".bamboo-div").css("border","hsl(176, 50%, 47%) solid 2px");
             $(".black-div").css("border","black solid 1px");
             $(".noReward-div").css("border","black solid 1px");
             //hide other pledge div
             document.querySelector(".blackEnterPledge").style.display = "none";
+            document.querySelector(".noEnterPledge").style.display = "none";
           } 
         //if selected value is black show black enter pledge div
             else if (selectedRadioBtn =="Black Edition Stand"){
-              document.querySelector(".blackEnterPledge").style.display = "block";
+              document.querySelector(".blackEnterPledge").style.display = "flex";
               //add green border to selected radio button
               $(".black-div").css("border","hsl(176, 50%, 47%) solid 2px");
               $(".bamboo-div").css("border","black solid 1px");
               $(".noReward-div").css("border","black solid 1px");
               //hide other pledge div
               document.querySelector(".bambooEnterPledge").style.display = "none";
+              document.querySelector(".noEnterPledge").style.display = "none";
             } 
             //if selected value is no reward add green border to no reward div
             else if (selectedRadioBtn =="No Reward") {
+              document.querySelector(".noEnterPledge").style.display = "flex";
               //add green border to selected radio button
               $(".noReward-div").css("border","hsl(176, 50%, 47%) solid 2px");
               $(".bamboo-div").css("border","black solid 1px");
               $(".black-div").css("border","black solid 1px");
+              //hide other pledge div
+              document.querySelector(".bambooEnterPledge").style.display = "none";
+              document.querySelector(".blackEnterPledge").style.display = "none";
             } 
             
     });
@@ -100,67 +106,77 @@ let completionBtn = document.querySelector(".completionBtn");
     $( "#radioForm" ).submit(function( event ) {
       completionModal.style.display="block";
 
-          //# of pledge is added to #backed and updated
-              // fetch total backed amount
+         // # of pledge is added to #backed and updated
+              //fetch total backed amount
               let totalBacked = document.querySelector('.totalBacked');
+              
               //turn into a num
               let totalBackedNum = parseInt(totalBacked.innerHTML);
-
-              //fetch bamboo pledge amount
-              let bambooPledgeAmount = document.querySelector("#bambooPledgeAmount").value;
-              
-              //fetch black pledge amount
-              let blackPledgeAmount = document.querySelector("#blackPledgeAmount").value;
-
-              let newTotalBacked = +totalBackedNum + +bambooPledgeAmount + +blackPledgeAmount;
-              
-              totalBacked.innerHTML = newTotalBacked;
-
-          //progress bar is updated based on above number
-
-              //fetch progressbar
-              let progressBar= document.querySelector("#progressBar");
-              //set value of progress bar to updated total
-              progressBar.value= newTotalBacked;
-
-                    //add 1 to total number of backers
-                      //fetch total number of backers
-                      let totalBackers= document.querySelector(".totalBackers");
-                      let totalBackersNum = parseInt(totalBackers.innerHTML);
-                      //each time form is submitted add 1 to total backers
-                      totalBackers.innerHTML = totalBackersNum + 1;
-
-              //# of items left updated
-                //fetch bamboo items
-                let totalBamboo = document.querySelectorAll('.bambooTotal');
-                //turn both into numbers
-                function totalBambooNum (){
-                  totalBamboo.forEach(item => {
-                    parseInt(item.innerHTML);
-                    //if bamboo pledge is greater than 0 subtract 1 from total
-                    if(bambooPledgeAmount >= 0) {
-                      item.innerHTML = parseInt(item.innerHTML) -1;
-                    }
-                })
-              }
-                totalBambooNum();
-
-                  //fetch black items
-                  let totalBlack = document.querySelectorAll('.blackTotal');
-                  //turn both into numbers
-                  function totalBlackNum() {
-                  totalBlack.forEach(black => {
-                    parseInt(black.innerHTML);
-                    //if black pledge is greater than 0 subtract 1 from total
-                    if(blackPledgeAmount >= 0) {
-                      black.innerHTML = parseInt(black.innerHTML) -1;
-                    }
-                  })
-                }
-                totalBlackNum();
+              console.log(totalBackedNum);
+                //fetch bamboo pledge amount
+                let bambooPledgeAmount = document.querySelector("#bambooPledgeAmount").value;
                 
+                //fetch black pledge amount
+                let blackPledgeAmount = document.querySelector("#blackPledgeAmount").value;
+                
+                let newTotalBacked = +totalBackedNum + +bambooPledgeAmount + +blackPledgeAmount;
+                console.log(new Intl.NumberFormat().format(newTotalBacked));
+                // totalBacked.innerHTML =  new Intl.NumberFormat().format(newTotalBacked);
+                totalBacked.innerHTML = newTotalBacked
 
+            //progress bar is updated based on above number
 
+                //fetch progressbar
+                let progressBar= document.querySelector("#progressBar");
+                //set value of progress bar to updated total
+                progressBar.value= newTotalBacked;
+
+                      //add 1 to total number of backers
+                        //fetch total number of backers
+                        let totalBackers= document.querySelector(".totalBackers");
+                        let totalBackersNum = parseInt(totalBackers.innerHTML);
+                        //each time form is submitted add 1 to total backers
+                        totalBackers.innerHTML = totalBackersNum + 1;
+
+                  //udpate total number of items left
+                    //fetch total #bamboo items
+                    let totalBambooItems = document.querySelectorAll(".bambooTotal")
+                    //fetch value of bamboo pledge
+                        //already got above
+                    //if bamboo pledge >0 subtract 1 from #bamboo items
+                    if(bambooPledgeAmount.length > 0){
+                      //set #bamboo items inner html = new number
+                      totalBambooItems.forEach(item => {
+                        item.innerHTML = parseInt(item.innerHTML) - 1;
+                      })
+                    } 
+                    else if (bambooPledgeAmount.length === 0) {
+                      totalBambooItems.forEach(item =>{
+                        item.innerHTML = parseInt(item.innerHTML);
+                      })
+                    }
+                    //fetch total #black items
+                    let totalBlackItems = document.querySelectorAll(".blackTotal");
+                    //fetch value of black pledge
+                        //already got above
+                    //turn into a num
+                    // let totalBlackItemNum = parseInt(totalBlackItems.innerHTML);
+                    //if black pledge >0 subtract 1 from #black items
+                    console.log(blackPledgeAmount.length);
+                    if(blackPledgeAmount.length > 0) {
+                        //set #black items inner html = new number
+                        totalBlackItems.forEach(item=>{
+                          item.innerHTML = parseInt(item.innerHTML) -1;
+                        })
+                    } 
+                    else if (blackPledgeAmount.length === 0) {
+                      totalBlackItems.forEach(item=> {
+                        item.innerHTML = parseInt(item.innerHTML);
+                      })
+
+                    }
+                    
+                    
 
       //prevent form from being submitted
       event.preventDefault();
@@ -173,17 +189,20 @@ let completionBtn = document.querySelector(".completionBtn");
             //hide extra divs
             document.querySelector(".bambooEnterPledge").style.display = "none";
             document.querySelector(".blackEnterPledge").style.display = "none";
+            document.querySelector(".noEnterPledge").style.display = "none";
             //reset all borders to black
             $(".noReward-div").css("border","black solid 1px");
             $(".bamboo-div").css("border","black solid 1px");
             $(".black-div").css("border","black solid 1px");
+
+            modal.style.display = "none";
+
       
 
     });
     //close completion modal when got it is clicked
     completionBtn.addEventListener("click", () => {
       completionModal.style.display = "none";
-      modal.style.display = "none";
     })
 
 
